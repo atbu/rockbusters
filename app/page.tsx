0d295668e44a2a0f4d8fbbb72820687715c9e3d5
atbu <186313861+atbu@users.noreply.github.com>
2025-03-02 21:57:38 +0000

    Change app metadata and add some text in the page centre

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Hello world!</h1>
+    <div className="h-screen flex items-center justify-center">
+      <p className="text-xl">Rockbusters</p>
     </div>
   );
 }
