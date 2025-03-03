ac98c183338a032309f60418228eef7195202392
atbu <186313861+atbu@users.noreply.github.com>
2025-03-03 13:07:32 +0000

    Set up experimental API route and fetch data from it through React

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-export default function Home() {
+export default async function Home() {
+
+  const data = await fetch('http://localhost:3000/api/getRandomRockbuster');
+  const rockbuster = await data.json();
+
   return (
     <div className="h-screen flex items-center justify-center">
       <p className="text-xl">Rockbusters</p>
+      <p>{rockbuster.test}</p>
     </div>
   );
+  
 }
