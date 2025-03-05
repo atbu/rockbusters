7bc66dc44a6f0291bdae827d35702bdc068237ee
atbu <186313861+atbu@users.noreply.github.com>
2025-03-05 08:19:13 +0000

    Make API route actually pull random data rather than just the first in DB

--- app/api/getRandomRockbuster/route.ts
+++ app/api/getRandomRockbuster/route.ts
@@ -2,15 +2,11 @@ import prisma from 'lib/prisma';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
-  const rockbuster = await prisma.rockbuster.findFirst();
-  const rockbusterClue = rockbuster?.clue;
-  const episodeId = rockbuster?.episodeId;
-  const episode = await prisma.episode.findUnique({
-    where: {
-      id: episodeId,
+  const rockbuster = await prisma.rockbuster.findRandom({
+    include: {
+      episode: true,
     }
   });
-  const episodeWinner = episode?.winner;
 
-  return new NextResponse(JSON.stringify({clue: rockbusterClue, winner: episodeWinner}), { status: 200 });
+  return new NextResponse(JSON.stringify(rockbuster), { status: 200 });
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,11 @@ export default async function Home() {
   const rockbuster = await data.json();
 
   return (
-    <div className="h-screen flex items-center justify-center">
+    <>
       <p className="text-xl">Rockbusters</p>
       <br />
-      <p>{rockbuster.clue}</p>
-      <br />
-      <p>{rockbuster.winner}</p>
-    </div>
+      <p>{rockbuster.episode.winner}</p>
+    </>
   );
   
 }
